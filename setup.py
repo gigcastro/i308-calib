8d61c069e9dc8cd92329306ea48c3e816cc1b10d
esteban <estebanuri@gmail.com>
2025-02-20 09:11:47 -0300

    calibration tool improvements WIP

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ setup(
     packages=find_packages(),
     install_requires=[
         "opencv-python",
-        "matplotlib"
+        "matplotlib",
+        "pyyaml"
     ],
     entry_points={
         'console_scripts': [
